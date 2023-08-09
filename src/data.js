ea7059b6bcf7dbc36afc82a7de04ac573746402b
Abhimanyu <mail2abhimanyu.gupta@gmail.com>
2023-08-09 19:42:40 +0530

    app component files

--- src/data.js
+++ src/data.js
@@ -1,22 +1,26 @@
-const projects = [
+export const projects = [
   {
+    id: '1',
     title: 'birthday buddy',
     url: 'https://react-vite-projects-1-birthday-buddy.netlify.app/',
-    image: './assets/birthday.png',
+    image: '/birthday.png',
   },
   {
+    id: '2',
     title: 'tours',
     url: 'https://react-vite-projects-2-tours.netlify.app/',
-    image: './assets/tours.png',
+    image: '/tours.png',
   },
   {
+    id: '3',
     title: 'reviews',
     url: 'https://react-vite-projects-3-reviews.netlify.app/',
-    image: './assets/reviews.png',
+    image: '/reviews.png',
   },
   {
+    id: '4',
     title: 'questions',
     url: 'https://react-vite-projects-4-accordion.netlify.app/',
-    image: './assets/questions.png',
+    image: '/questions.png',
   },
 ];
